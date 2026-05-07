8b11e0e7905af6aae0f2200479285a28990d0430
Simon Dina <simondina@hotmail.com>
2026-05-07 14:48:46 +0100

    feat: initial repo structure and SwiftUI iOS app

---
+++ ios/EcoHomeApp/EcoHomeApp/EcoHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoHomeAppApp.swift
+//  EcoHomeApp
+//
+//  Created by sdina on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct EcoHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
